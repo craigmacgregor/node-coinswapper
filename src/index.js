09be0a9f383eb06a1525706bd5075120bd480414
Craig <craig.b.macgregor@gmail.com>
2016-05-22 13:24:53 +1200

    gitignore and some new tests

--- src/index.js
+++ src/index.js
@@ -1,23 +1,55 @@
-const express = require('express')
-const bitcoin = require('bitcoin')
-const app = express()
+'use strict'
 
-app.get('/', function (req, res) {
+const Client = require('bitcoin-core')
 
-  const client = new bitcoin.Client({
-    host: 'localhost',
-    port: 8332,
-    user: 'username',
-    pass: 'password',
-    timeout: 30000,
-  })
+/* SETTINGS */
 
-  client.getBalance('*', 6, (err, balance, resHeaders) => {
-    if (err) return console.log(err)
-    res.status(200).send('Balance: ' + balance)
-  })
+const nav1Receiving = 'sJgrkHqvxYD1AKhcUKCnetVVdL3TqcDysr'
+const nav1RcpUser = '*'
+const nav1RcpPass = '*'
+const rcpPort = 44444
+
+const nav2RcpUser = '*'
+const nav2RcpPass = '*'
+const nav2IPAddress = '192.168.1.8'
+/* INIT */
+
+const localNav1Client = new Client({
+  username: nav1RcpUser,
+  password: nav1RcpPass,
+  port: rcpPort,
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+const remoteNav2Client = new Client({
+  username: nav2RcpUser,
+  password: nav2RcpPass,
+  port: rcpPort,
+  host: nav2IPAddress,
 })
+
+remoteNav2Client.getInfo().then((info) => console.log('remoteNav2Client', info))
+
+localNav1Client.getInfo().then((info) => getUnspent(info))
+
+const getUnspent = (info) => {
+  console.log('localNav1Client', info)
+  localNav1Client.listUnspent().then((unspent) => filterUnspent(unspent))
+}
+
+const filterUnspent = (unspent) => {
+  // console.log('listUnspent', unspent)
+  let hasPending = false
+
+  for (const pending of unspent) {
+    if (pending.address === nav1Receiving) {
+      hasPending = true
+      processTransaction(pending)
+    }
+  }
+
+  if (!hasPending) console.log('nothing to process')
+}
+
+const processTransaction = (pending) => {
+  console.log('processTransaction', pending)
+}
